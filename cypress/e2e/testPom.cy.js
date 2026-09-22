0bb70dac8cdf4436d444af91daee8022ff56b502
arinta <arintaribka@gmail.com>
2026-09-23 02:25:37 +0700

    Rapikan format kode dengan Prettier

--- cypress/e2e/testPom.cy.js
+++ cypress/e2e/testPom.cy.js
@@ -1,40 +1,40 @@
 import loginPage from '../support/loginPage'
 import loginData from '../fixtures/logindata.json'
 
-describe ('Verifikasi fungsi Login', () =>{
+describe('Verifikasi fungsi Login', () => {
     it('TC=001 - Login dengan username valid dan password valid', () => {
-    loginPage.masukHalaman()
-    loginPage.masukUsername(loginData.usernameValid)
-    loginPage.masukanPassword(loginData.passwordValid)
-    loginPage.klikLogin()
+        loginPage.masukHalaman()
+        loginPage.masukUsername(loginData.usernameValid)
+        loginPage.masukanPassword(loginData.passwordValid)
+        loginPage.klikLogin()
 
-    loginPage.VerifikasiUrl('/dashboard/index')  
+        loginPage.VerifikasiUrl('/dashboard/index')
     })
 
-   it('TC=002 - Login dengan username yang belum terdaftar', () => {
-    loginPage.masukHalaman()
-    loginPage.masukUsername(loginData.usernameInvalid)
-    loginPage.masukanPassword(loginData.passwordValid)
-    loginPage.klikLogin()
+    it('TC=002 - Login dengan username yang belum terdaftar', () => {
+        loginPage.masukHalaman()
+        loginPage.masukUsername(loginData.usernameInvalid)
+        loginPage.masukanPassword(loginData.passwordValid)
+        loginPage.klikLogin()
 
-    loginPage.VerifikasiPesanError('Invalid credentials')   
+        loginPage.VerifikasiPesanError('Invalid credentials')
     })
 
     it('TC=003 - Login dengan password yang salah', () => {
-    loginPage.masukHalaman()
-    loginPage.masukUsername(loginData.usernameValid)
-    loginPage.masukanPassword(loginData.passwordInvalid)
-    loginPage.klikLogin()
-
-    loginPage.VerifikasiPesanError('Invalid credentials')   
-    })
-
-     it('TC=004 - Login dengan username valid tapi password salah', () => {
         loginPage.masukHalaman()
         loginPage.masukUsername(loginData.usernameValid)
         loginPage.masukanPassword(loginData.passwordInvalid)
         loginPage.klikLogin()
- 
+
+        loginPage.VerifikasiPesanError('Invalid credentials')
+    })
+
+    it('TC=004 - Login dengan username valid tapi password salah', () => {
+        loginPage.masukHalaman()
+        loginPage.masukUsername(loginData.usernameValid)
+        loginPage.masukanPassword(loginData.passwordInvalid)
+        loginPage.klikLogin()
+
         loginPage.VerifikasiPesanError('Invalid credentials')
     })
 
@@ -43,51 +43,37 @@ describe ('Verifikasi fungsi Login', () =>{
         loginPage.masukUsername(loginData.usernameKosong)
         loginPage.masukanPassword(loginData.passwordKosong)
         loginPage.klikLogin()
- 
+
         loginPage.VerifikasiJumlahPesanRequired(2)
     })
- 
+
     it('TC=006 - Login dengan username kosong, password terisi', () => {
         loginPage.masukHalaman()
         loginPage.masukUsername(loginData.usernameKosong)
         loginPage.masukanPassword(loginData.passwordValid)
         loginPage.klikLogin()
- 
+
         loginPage.VerifikasiJumlahPesanRequired(1)
     })
- 
+
     it('TC=007 - Login dengan password kosong, username terisi', () => {
         loginPage.masukHalaman()
         loginPage.masukUsername(loginData.usernameValid)
         loginPage.masukanPassword(loginData.passwordKosong)
         loginPage.klikLogin()
- 
+
         loginPage.VerifikasiJumlahPesanRequired(1)
     })
- 
+
     it('TC=008 - Verifikasi elemen halaman login tampil dengan benar', () => {
         loginPage.masukHalaman()
- 
+
         loginPage.VerifikasiElemenHalamanLogin()
     })
 
-
-
-
-
-
-
-
-
-
-
-
-
-        //cy.url().should('include','tory')
-        //cy.visit('https://opensource-demo.orangehrmlive.com/')
-        //cy.get('[name="username"]').type('Admin')
-        //cy.get('button[type="submit"]').click()
-         //cy.url().should('include','tory')
-    
-
+    //cy.url().should('include','tory')
+    //cy.visit('https://opensource-demo.orangehrmlive.com/')
+    //cy.get('[name="username"]').type('Admin')
+    //cy.get('button[type="submit"]').click()
+    //cy.url().should('include','tory')
 })
--- cypress/support/loginPage.js
+++ cypress/support/loginPage.js
@@ -1,32 +1,38 @@
-class loginPage{
-    masukHalaman(){
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+class loginPage {
+    masukHalaman() {
+        cy.visit(
+            'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
+        )
     }
-    masukUsername(username){
+    masukUsername(username) {
         username && cy.get('[name="username"]').type(username)
     }
-        masukanPassword(password){
+    masukanPassword(password) {
         password && cy.get('[name="password"]').type(password)
     }
-    klikLogin(){
+    klikLogin() {
         cy.get('button[type="submit"]').click()
     }
-    VerifikasiUrl(pathHarapan){
+    VerifikasiUrl(pathHarapan) {
         cy.url().should('include', pathHarapan)
     }
-    VerifikasiPesanError(pesanHarapan){
-        cy.get('.oxd-alert-content-text').should('be.visible').and('contain.text', pesanHarapan)
+    VerifikasiPesanError(pesanHarapan) {
+        cy.get('.oxd-alert-content-text')
+            .should('be.visible')
+            .and('contain.text', pesanHarapan)
     }
-    VerifikasiJumlahPesanRequired(jumlah){
+    VerifikasiJumlahPesanRequired(jumlah) {
         cy.get('.oxd-input-field-error-message').should('have.length', jumlah)
     }
-     klikLupaPassword(){
+    klikLupaPassword() {
         cy.get('.orangehrm-login-forgot-header').click()
     }
-     VerifikasiHalamanLupaPassword(){
-        cy.get('.orangehrm-forgot-password-title').should('be.visible').and('contain.text', 'Reset Password')
+    VerifikasiHalamanLupaPassword() {
+        cy.get('.orangehrm-forgot-password-title')
+            .should('be.visible')
+            .and('contain.text', 'Reset Password')
     }
-    VerifikasiElemenHalamanLogin(){
+    VerifikasiElemenHalamanLogin() {
         cy.get('.orangehrm-login-branding img').should('be.visible')
         cy.get('.orangehrm-login-form').should('be.visible')
         cy.get('[name="username"]').should('be.visible')
